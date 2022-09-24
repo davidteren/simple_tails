7f1f62d17f02f0fccf0b2237d67007628568eeee
davidteren <drteren@gmail.com>
2022-09-24 14:44:10 +0200

    add posts

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,5 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,7 +3,17 @@ import AnimatedNumber from 'stimulus-animated-number'
 import CharacterCounter from 'stimulus-character-counter'
 
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
 application.register('animated-number', AnimatedNumber)
 application.register('character-counter', CharacterCounter)
+
+
+// Import and register all TailwindCSS Components
+import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
+application.register('alert', Alert)
+application.register('autosave', Autosave)
+application.register('dropdown', Dropdown)
+application.register('modal', Modal)
+application.register('tabs', Tabs)
+application.register('popover', Popover)
+application.register('toggle', Toggle)
+application.register('slideover', Slideover)
