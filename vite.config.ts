bc5e8a1e9eb84e28c82e99aca0212d87955f57e9
davidteren <drteren@gmail.com>
2022-09-25 16:51:39 +0200

    add vite-plugin-stimulus-hmr

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import {defineConfig} from 'vite'
-import FullReload from "vite-plugin-full-reload";
+import FullReload from "vite-plugin-full-reload"
 import RubyPlugin from 'vite-plugin-ruby'
+import StimulusHMR from 'vite-plugin-stimulus-hmr'
 
 export default defineConfig({
       clearScreen: false,
       plugins: [
         RubyPlugin(),
+          StimulusHMR(),
         FullReload(["config/routes.rb", "app/views/**/*"], {delay: 300}),
       ],
-
     }
 )
