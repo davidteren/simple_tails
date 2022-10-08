d84916989b11fe4c05fb85529ce25edc5fcd68fc
davidteren <drteren@gmail.com>
2022-10-09 00:58:03 +0200

    comment out Turbocontroller for now

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -16,24 +16,24 @@ class TurboFailureApp < Devise::FailureApp
   end
 end
 
-class TurboController < ApplicationController
-  class Responder < ActionController::Responder
-    def to_turbo_stream
-      controller.render(options.merge(formats: :html))
-    rescue ActionView::MissingTemplate => error
-      if get?
-        raise error
-      elsif has_errors? && default_action
-        render rendering_options.merge(formats: :html, status: :unprocessable_entity)
-      else
-        redirect_to navigation_location
-      end
-    end
-  end
-
-  self.responder = Responder
-  respond_to :html, :turbo_stream
-end
+# class TurboController < ApplicationController
+#   class Responder < ActionController::Responder
+#     def to_turbo_stream
+#       controller.render(options.merge(formats: :html))
+#     rescue ActionView::MissingTemplate => error
+#       if get?
+#         raise error
+#       elsif has_errors? && default_action
+#         render rendering_options.merge(formats: :html, status: :unprocessable_entity)
+#       else
+#         redirect_to navigation_location
+#       end
+#     end
+#   end
+#
+#   self.responder = Responder
+#   respond_to :html, :turbo_stream
+# end
 
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
@@ -47,7 +47,7 @@ Devise.setup do |config|
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
-  config.parent_controller = 'TurboController'
+  # config.parent_controller = 'TurboController'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
